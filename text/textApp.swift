fab4f1a116c7f53e7053a99fbba4ac3def617e8f
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-20 13:21:04 -0700

    Initial Commit

---
+++ text/textApp.swift
@@ -0,0 +1,17 @@
+//
+//  textApp.swift
+//  text
+//
+//  Created by user on 6/20/21.
+//
+
+import SwiftUI
+
+@main
+struct textApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
